131ef629c7189a5bf6bf1cb2361b7cf3f2be3945
Manu Wallner <m.wallner@xforge.at>
2016-10-22 10:52:00 -0700

    Road53 config

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,5 +1,74 @@
 provider "aws" {
- 	region     = "us-west-2"
+ 	region     = "eu-central-1"
     access_key = "${var.aws_access_key}"
     secret_key = "${var.aws_secret_key}"
+}
+
+resource "aws_s3_bucket" "s3-website-bucket" {
+  bucket = "supermil.ch-site"
+  acl = "public-read"
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = "${aws_s3_bucket.s3-website-bucket.id}.s3.amazonaws.com"
+    origin_id   = "myS3Origin"
+
+    s3_origin_config { 
+      origin_access_identity = ""
+    }
+  }
+
+  enabled             = true
+  comment             = "Cloudfront"
+  default_root_object = "index.html"
+
+  aliases = ["supermil.ch"]
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "myS3Origin"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  price_class = "PriceClass_All"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+resource "aws_route53_zone" "site" {
+   name = "supermil.ch."
+}
+
+resource "aws_route53_record" "site" {
+  zone_id = "${aws_route53_zone.site.zone_id}"
+  name = "supermil.ch"
+  type = "A"
+
+  alias {
+    name = "${aws_cloudfront_distribution.s3_distribution.domain_name}"
+    zone_id = "${aws_cloudfront_distribution.s3_distribution.hosted_zone_id}"
+    evaluate_target_health = true
+  }
 }
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,2 +1,14 @@
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
+
+output "cloudfront-dns" {
+  value = "${aws_cloudfront_distribution.s3_distribution.domain_name}"
+}
+
+output "s3-bucket" {
+  value = "${aws_s3_bucket.s3-website-bucket.id}.s3.amazonaws.com"
+}
+
+output "site" {
+  value = "${aws_route53_record.site.fqdn}"
+}
